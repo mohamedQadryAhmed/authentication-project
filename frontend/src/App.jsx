41aa4076ee7f93c0596067ae538c2566d9d77671
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2026-01-28 14:28:23 +0200

    feat: setup React with Tailwind CSS and initial template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+      <div className='bg-white p-8 rounded-xl shadow-md w-full max-w-md'>
+        <h1 className='text-2xl font-bold text-center mb-6'>Auth System</h1>
+
+        <p className='text-center text-gray-600'>
+          React + TailwindCSS starter template
+        </p>
+
+        <button className='mt-6 w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition'>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
